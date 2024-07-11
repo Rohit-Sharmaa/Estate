2f8eccde4aa48693b28d5ab2c085aa385137777a
Rohit-Sharmaa <iamrohit3000@gmail.com>
2024-07-11 09:37:26 +0530

    created basic pages and routes for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import SignUp from "./SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 
 function App() {
-  return <div className="text-red-500">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          {" "}
+        </Route>
+        <Route path="/sign-in" element={<Signin />}>
+          {" "}
+        </Route>
+        <Route path="/sign-up" element={<SignUp />}>
+          {" "}
+        </Route>
+        <Route path="/about" element={<About />}>
+          {" "}
+        </Route>
+        <Route path="/profile" element={<Profile />}>
+          {" "}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Signin.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+function Signin() {
+  return <div>Signin</div>;
+}
+
+export default Signin;
