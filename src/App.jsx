529686e10b65b7d1ac40092970ebe2741d8cb6d4
Rohit-Sharmaa <iamrohit3000@gmail.com>
2024-07-13 06:22:52 +0530

    added header componenet

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Signin from "./pages/Signin";
 import SignUp from "./SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}>
           {" "}
