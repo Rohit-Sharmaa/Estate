f182bf2092445bb9f1bbb50ee7837a1b6c872b1e
Rohit-Sharmaa <iamrohit3000@gmail.com>
2024-07-14 09:07:09 +0530

    connected to db

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,17 @@
 import expres from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.ConnectionString)
+  .then(() => {
+    console.log("Connected to MongoDb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = expres();
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("server is running on port 3000");
 });
