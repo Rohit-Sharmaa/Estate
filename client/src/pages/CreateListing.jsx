6dc00662618b1517445dc3601f7f9c8310727b52
Rohit-Sharmaa <iamrohit3000@gmail.com>
2024-07-19 23:43:54 +0530

    added some checks in the create listing page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -21,7 +21,7 @@ export default function CreateListing() {
     type: "rent",
     bedrooms: 1,
     bathrooms: 1,
-    regularPrice: 50,
+    regularPrice: 5,
     discountPrice: 0,
     offer: false,
     parking: false,
@@ -272,7 +272,7 @@ export default function CreateListing() {
               <input
                 type="number"
                 id="regularPrice"
-                min="50"
+                min="5"
                 max="10000000"
                 required
                 className="p-3 border border-gray-300 rounded-lg"
