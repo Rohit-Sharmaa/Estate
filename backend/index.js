080ef58568e78c5e95db093946fd2454fc64e225
Rohit-Sharmaa <iamrohit3000@gmail.com>
2024-07-24 07:07:21 +0530

    added commands for installations

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
 import listingRouter from "./routes/listing.route.js";
+import path from "path";
 dotenv.config();
 mongoose
   .connect(process.env.ConnectionString)
@@ -14,6 +15,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+const __dirname = path.resolve();
+
 const app = expres();
 app.use(expres.json());
 app.use(cookieParser());
@@ -25,6 +28,12 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
